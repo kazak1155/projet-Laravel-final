a61678569581428b2746c49823478d7d0f2e4a5d
Andrey Volynkin <kazakmt4@gmail.com>
2023-06-15 14:58:28 +0300

    make model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
@@ -28,5 +29,10 @@ class Room extends Model
     public function bookings() : HasMany
     {
         return $this->hasMany(Booking::class);
+    }
+
+    public function facilites(): BelongsToMany
+    {
+        return $this->belongsToMany(Facility::class);
     }
 }
